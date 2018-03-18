16874a5a77548243dbc7a6f94575888bd0b0c252
LarsErik <LarsErik@DESKTOP-O2VKKO7>
2018-03-18 20:52:13 +0100

    Initial commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Sensorcard_with_FTDI.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Sensorcard_with_FTDI.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=sensorcardwithftdi.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sensorcardwithftdi.x.tar
